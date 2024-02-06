b74fdc89f84f9733f406054f9d0a033aac97509f
kenethriera <kenethrr@outlook.com>
2024-02-06 16:50:57 -0500

    Agrega métodos adicionales para obtener datos de productos en interfaz

--- app/product/repositories/productRepository.go
+++ app/product/repositories/productRepository.go
@@ -4,4 +4,10 @@ import "myclothing/app/product/entities"
 
 type ProductRepository interface {
 	SelectProducts() ([]entities.Product, error)
+	SelectProductCategoryById(categoryId int32) (*entities.ProductCategory, error)
+	SelectProductSubcategoryById(subcategoryId int32) (*entities.ProductSubcategory, error)
+	SelectProductSubcategoryByCategoryId(categoryId int32) ([]entities.ProductSubcategory, error)
+	SelectProductSizeByCode(sizeCode string) (*entities.ProductSize, error)
+	SelectProductColorById(colorId int32) (*entities.ProductColor, error)
+	SelectProductSourceById(sourceId int32) (*entities.ProductSource, error)
 }
